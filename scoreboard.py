2a57c25e8a567bae9090a7984e1d2965e207299a
Hakan Tetik <hakantetik@gmail.com>
2021-10-10 13:15:35 +0300

    high score save to text added. Now high score will be saved

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,10 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
+
+with open("high_score.txt", mode="r") as file:
+    high_score = file.read()
 
 
 class Scoreboard(Turtle):
@@ -8,7 +12,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        self.highscore = high_score
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -20,11 +24,12 @@ class Scoreboard(Turtle):
         self.write(f"Score: {self.score}, High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def reset_(self):
-        if self.score > self.highscore:
+        if self.score > int(self.highscore):
             self.highscore = self.score
+            with open("high_score.txt", mode="w") as file:
+                file.write(str(self.highscore))
         self.score = 0
         self.update_scoreboard()
-
 
     def increase_score(self):
         self.score += 1
